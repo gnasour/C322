bf4e8a00098dc576d97185073a01b5f271c3c2b6
gnasour <gnasour@users.noreply.github.com>
2017-02-22 14:02:19 -0800

    Add files via upload
    Lab2 Commit and Push

---
+++ lab2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void main(int argc,char *argv[])
+{
+  pid_t pid;
+  int system(const char *command);
+  int parentPID = getpid();
+  int fileCounter=1;
+  int newARGC = argc;
+
+  for(argc; argc > 1; argc--){
+   pid =  fork();
+   if( pid < 0){
+     printf("ERROR IN FORK\n");
+   }//error
+   else if(pid == 0){
+      printf("Filename: %s \t  PID: %d\n", argv[fileCounter], getpid());
+      sleep(1);
+      exit(0);
+   }//child if
+   fileCounter++;
+  }//for loop
+  for(newARGC; newARGC > 1; newARGC--){
+    wait();
+  }//wait for loop
+  //  if(getpid()==parentPID){
+  system("ps -H");
+  printf("DONE\n");
+    //}
+}//main
